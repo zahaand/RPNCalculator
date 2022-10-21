5a90845579c00ee7d5e2426eb2fe10b4be0a1fd9
Andrey Zakharenkov <zahaand@gmail.com>
2022-10-21 23:08:53 +0300

    add returnRPNMathExpressionWithBrasses test

--- src/test/java/ru/zahaand/service/RPNConverterTest.java
+++ src/test/java/ru/zahaand/service/RPNConverterTest.java
@@ -41,4 +41,12 @@ class RPNConverterTest {
         Assert.assertTrue(Arrays.equals(new String[]{"3", "7", "10", "*", "+"}, answer));
     }
 
+    @Test
+    void returnRPNMathExpressionWithBrasses() {
+        Mockito.when(reader.read()).thenReturn("(3 + 7) - 5");
+        String[] answer = converter.convert("(3 + 7) * 5");
+
+        Assert.assertTrue(Arrays.equals(new String[]{"3", "7", "+", "5", "*"}, answer));
+    }
+
 }
