be8e963e36a9309ce3460eff52ef0c706d21ec85
Andrey Zakharenkov <zahaand@gmail.com>
2022-10-21 13:24:26 +0300

    realize calculate() method

--- src/main/java/ru/zahaand/Calculator.java
+++ src/main/java/ru/zahaand/Calculator.java
@@ -1,5 +1,7 @@
 package ru.zahaand;
 
+import java.util.Stack;
+
 public class Calculator {
     Reader reader;
     Converter converter;
@@ -12,6 +14,37 @@ public class Calculator {
     public double calculate() {
         String mathExpression = reader.read();
         String[] mathExpressionElements = converter.convert(mathExpression);
-        return 10.0;
+        Stack<Double> stack = new Stack<>();
+
+        for (String element : mathExpressionElements) {
+            /*
+            if the element is numeric, push the element to the top of the stack
+            else get two numbers from the top of the stack and perform the math operation
+             */
+            if (isNumber(element)) {
+                stack.push(Double.valueOf(element));
+            } else {
+                double popNumber1 = stack.pop();
+                double popNumber2 = stack.pop();
+                switch (element) {
+                    case "+" -> stack.push(popNumber2 + popNumber1);
+                    case "-" -> stack.push(popNumber2 - popNumber1);
+                    case "*" -> stack.push(popNumber2 * popNumber1);
+                    case "/" -> stack.push(popNumber2 / popNumber1);
+                    case "%" -> stack.push(popNumber2 % popNumber1);
+                    case "^" -> stack.push(Math.pow(popNumber2, popNumber1));
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    private boolean isNumber(String mathExpressionElement) {
+        try {
+            Integer.parseInt(mathExpressionElement);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- src/test/java/ru/zahaand/CalculatorTest.java
+++ src/test/java/ru/zahaand/CalculatorTest.java
@@ -10,7 +10,11 @@ class CalculatorTest {
         Reader reader = Mockito.mock(Reader.class);
         Converter converter = Mockito.mock(Converter.class);
         Calculator calculator = new Calculator(reader, converter);
+
+        Mockito.when(reader.read()).thenReturn("3+7");
+        Mockito.when(converter.convert("3+7")).thenReturn(new String[]{"3", "7", "+"});
         double answer = calculator.calculate();
+
         Assert.assertEquals(10.0, answer);
     }
 }
