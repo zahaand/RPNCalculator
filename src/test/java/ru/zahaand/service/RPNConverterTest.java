b7244259e6a57de4de3fde048c228b6346d47d80
Andrey Zakharenkov <zahaand@gmail.com>
2022-10-21 23:59:00 +0300

    add returnRPNMathExpressionWithNegativeNumbers test

--- src/test/java/ru/zahaand/service/RPNConverterTest.java
+++ src/test/java/ru/zahaand/service/RPNConverterTest.java
@@ -49,4 +49,11 @@ class RPNConverterTest {
         Assert.assertTrue(Arrays.equals(new String[]{"3", "7", "+", "5", "*"}, answer));
     }
 
+    @Test
+    void returnRPNMathExpressionWithNegativeNumbers() {
+        Mockito.when(reader.read()).thenReturn("(-3 + 7) + (-5)");
+        String[] answer = converter.convert("(-3 + 7) + (-5)");
+
+        Assert.assertTrue(Arrays.equals(new String[]{"0", "3", "-,", "7", "+", "0", "5", "-", "+"}, answer));
+    }
 }
