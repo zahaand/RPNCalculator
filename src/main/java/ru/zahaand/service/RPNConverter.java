293cbced8943091d0257fd0a63344cde92f09436
Andrey Zakharenkov <zahaand@gmail.com>
2022-10-21 22:20:14 +0300

    realize convert() method

--- src/main/java/ru/zahaand/service/RPNConverter.java
+++ src/main/java/ru/zahaand/service/RPNConverter.java
@@ -1,8 +1,48 @@
 package ru.zahaand.service;
 
+import java.util.Stack;
+
 public class RPNConverter implements Converter {
     @Override
     public String[] convert(String mathExpression) {
-        return new String[]{"3", "7", "+"};
+        StringBuilder mathExpressionBuilder = new StringBuilder();
+        Stack<Character> mathCharactersStack = new Stack<>();
+        char[] mathExpressionCharacters = mathExpression.replaceAll("\\s", "").toCharArray();
+
+        for (Character character : mathExpressionCharacters) {
+            if (Character.isDigit(character)) {
+                mathExpressionBuilder.append(character);
+            } else {
+                mathExpressionBuilder.append(" ");
+                appendCharacterToMathExpression(mathExpressionBuilder, character, mathCharactersStack);
+            }
+        }
+        while (!mathCharactersStack.isEmpty()) {
+            mathExpressionBuilder.append(" ").append(mathCharactersStack.pop());
+        }
+        return mathExpressionBuilder.toString().trim().split(" ");
+    }
+
+    private void appendCharacterToMathExpression(StringBuilder builder, Character character, Stack<Character> stack) {
+        int mathCharacterPriority = getPriority(character);
+        if (!stack.isEmpty()) {
+            int stackTopMathCharacterPriority = getPriority(stack.peek());
+            if (stackTopMathCharacterPriority >= mathCharacterPriority) {
+                builder.append(character);
+            }
+        } else {
+            stack.push(character);
+        }
+    }
+
+    private int getPriority(Character character) {
+        switch (character) {
+            case '+', '-' -> {
+                return 1;
+            }
+            default -> {
+                return 2;
+            }
+        }
     }
 }
